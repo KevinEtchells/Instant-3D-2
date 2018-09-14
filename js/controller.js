8622f84ff8f76b4d285028e8bb7e17a860961178
Kevin Etchells <kevetchells@hotmail.com>
2018-09-14 13:52:59 +0100

    Add basic gobo support

--- js/controller.js
+++ js/controller.js
@@ -17,7 +17,7 @@ var vm;
             width: 40,
             items: [
                 {type: "stage", width: 40, height: 2, depth: 8},
-                {type: "set", width: 40, height: 11, bottom: 2, back: -4, lighting: "#5511EE"},
+                {type: "set", width: 40, height: 11, bottom: 2, back: -4, lighting: "#0b63f4"},
                 {type: "screen", width: 16, height: 9, bottom: 5.5, back: -3, surround: 0.33}
             ]
         },
@@ -45,6 +45,30 @@ var vm;
                     } 
                 });
                 render();
+            },
+            
+            addItem: function (type) {
+                this.items.push({
+                    id: Date.now(),
+                    type: type,
+                    width: 8,
+                    height: 4,
+                    bottom: 7,
+                    left: 15,
+                    back: -3
+                });
+            },
+            
+            removeItem: function (id) {
+                var foundIndex = -1;
+                this.items.forEach(function (item, itemIndex) {
+                    if (item.id && item.id === id) {
+                        foundIndex = itemIndex;
+                    }
+                });
+                if (foundIndex !== -1) {
+                    this.items.splice(foundIndex, 1);
+                }
             }
             
         },
--- js/render.js
+++ js/render.js
@@ -26,22 +26,33 @@ var render;
     
         vm.items.forEach(function (item) {
 
-            var $items = [document.createElement("a-box")];
-
-            $items[0].setAttribute("width", item.width);
-            $items[0].setAttribute("height", item.height);
-            $items[0].setAttribute("position", "0 " + ((item.bottom || 0) + (item.height / 2)) + " " + (item.back || 0));
+            var $items = [];
 
             if (item.type === "stage") {
+                $items.push(document.createElement("a-box"));
+                $items[0].setAttribute("width", item.width);
+                $items[0].setAttribute("height", item.height);
+                $items[0].setAttribute("position", "0 " + ((item.bottom || 0) + (item.height / 2)) + " " + (item.back || 0));
                 $items[0].setAttribute("color", "#111");
                 $items[0].setAttribute("depth", item.depth);
+                
             } else if (item.type === "set") {
+                $items.push(document.createElement("a-box"));
+                $items[0].setAttribute("width", item.width);
+                $items[0].setAttribute("height", item.height);
+                $items[0].setAttribute("position", "0 " + ((item.bottom || 0) + (item.height / 2)) + " " + (item.back || 0));
                 $items[0].setAttribute("color", item.lighting || "#FFF");
                 $items[0].setAttribute("src", "assets/uplighters.png");
                 $items[0].setAttribute("depth", "0.2");
-            } else if (item.type === "screen" || item.type === "graphic-printed") {
+
+            } else if (item.type === "screen") {
+                $items.push(document.createElement("a-box"));
+                $items[0].setAttribute("width", item.width);
+                $items[0].setAttribute("height", item.height);
+                $items[0].setAttribute("position", "0 " + ((item.bottom || 0) + (item.height / 2)) + " " + (item.back || 0));
                 $items[0].setAttribute("color", "#FFF");
                 $items[0].setAttribute("depth", "0.1");
+
                 if (item.surround) {
                     $items.push(document.createElement("a-box"));
                     $items[1] = document.createElement("a-box");
@@ -51,6 +62,14 @@ var render;
                     $items[1].setAttribute("depth", "0.1");
                     $items[1].setAttribute("position", "0 " + ((item.bottom || 0) + (item.height / 2)) + " " + ((item.back || 0) - 0.1));
                 }
+
+            } else if (item.type === "graphic") {
+                $items.push(document.createElement("a-image"));
+                $items[0].setAttribute("opacity", "0.8");
+                $items[0].setAttribute("width", item.width);
+                $items[0].setAttribute("height", item.height);
+                $items[0].setAttribute("position", (item.left || 0) + " " + ((item.bottom || 0) + (item.height / 2)) + " " + ((item.back || 0) + 0.1));
+                $items[0].setAttribute("src", "user-content/" + item.src);
             }
 
             $items.forEach(function ($item) {
